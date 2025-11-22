14d4d6b0ad071144baf608eacc173534dbc6d74d
Vinícius Hoyer <contact@vhoyer.dev>
2025-11-22 11:58:59 -0300

    ✨ Add velocity meter

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -17,6 +17,9 @@ var camera_main: Camera3D = %CameraMain
 @onready
 var camera_base_look_at: Marker3D = %BaseCameraLookAt
 
+@onready
+var velocity_label: Label = %Velocity
+
 
 var look_at_pos: Vector3
 
@@ -39,3 +42,6 @@ func _physics_process(delta: float) -> void:
 
 	look_at_pos = look_at_pos.lerp(camera_base_look_at.global_position + self.linear_velocity, delta * 5.0)
 	camera_main.look_at(look_at_pos)
+
+	## debug
+	velocity_label.text = '%.3f m/s' % [self.linear_velocity.length()]
